dd6e93c8601b6be26992a7bb38fe3b1b4cf9be94
WUCH8 <944204910@qq.com>
2020-08-26 21:25:39 +0800

    [mocking][jayden chris]test:testIsAllowOverPark_givenCarNameDoesNotContainsCharacterAAndIsVipCar_thenReturnFalse

--- mocking/src/test/java/parking/VipParkingStrategyTest.java
+++ mocking/src/test/java/parking/VipParkingStrategyTest.java
@@ -9,6 +9,7 @@ import org.mockito.Mock;
 import org.mockito.runners.MockitoJUnitRunner;
 
 import static org.junit.Assert.assertEquals;
+import static org.junit.Assert.assertFalse;
 import static org.junit.Assert.assertTrue;
 import static org.mockito.Mockito.*;
 
@@ -81,6 +82,14 @@ public class VipParkingStrategyTest {
          * You may refactor the code, or try to use
          * use @RunWith(MockitoJUnitRunner.class), @Mock (use Mockito, not PowerMock) and @InjectMocks
          */
+        //given
+        Car car = new Car("1");
+
+        //when
+        boolean result = vipParkingStrategy.isAllowOverPark(car);
+
+        //then
+        assertFalse(result);
     }
 
     @Test
