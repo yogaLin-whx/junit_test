55dcedc0ba47b3786ab2ea11b93ada01d104065a
WUCH8 <944204910@qq.com>
2020-08-26 21:59:19 +0800

    [mocking][jayden chris]test:testCalculateHourlyPrice_givenSunday_thenPriceIsDoubleOfSundayPrice

--- mocking/src/test/java/parking/VipParkingStrategyPowerMockTest.java
+++ mocking/src/test/java/parking/VipParkingStrategyPowerMockTest.java
@@ -1,25 +1,44 @@
 package parking;
 
-import org.junit.Test;
+import static org.junit.Assert.assertEquals;
+import static org.powermock.api.mockito.PowerMockito.mockStatic;
 
 import java.util.Calendar;
+import org.junit.Test;
+import org.junit.runner.RunWith;
+import org.mockito.Mockito;
+import org.powermock.api.mockito.PowerMockito;
+import org.powermock.core.classloader.annotations.PrepareForTest;
+import org.powermock.modules.junit4.PowerMockRunner;
 
+@RunWith(PowerMockRunner.class)
+@PrepareForTest(value = {ParkingLot.class, Calendar.class})
 public class VipParkingStrategyPowerMockTest {
 
-    @Test
-    public void testCalculateHourlyPrice_givenSunday_thenPriceIsDoubleOfSundayPrice(){
+  @Test
+  public void testCalculateHourlyPrice_givenSunday_thenPriceIsDoubleOfSundayPrice()
+      throws Exception {
 
-        /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
-        * by using PowerMock to mock static method */
+    /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
+     * by using PowerMock to mock static method */
+    //given
+    Calendar endOfMarch = Calendar.getInstance();
+    endOfMarch.set(2020, Calendar.AUGUST, 30);
+    PowerMockito.mockStatic(Calendar.class);
+    Mockito.when(Calendar.getInstance()).thenReturn(endOfMarch);
 
-    }
+    //when
+    VipParkingStrategy vipParkingStrategy = new VipParkingStrategy();
+    int result = vipParkingStrategy.calculateHourlyPrice();
 
-    @Test
-    public void testCalculateHourlyPrice_givenNotSunday_thenPriceIsDoubleOfNonSundayPrice(){
+    assertEquals(50, result);
+  }
 
-        /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
-         * by using PowerMock to mock static method */
+  @Test
+  public void testCalculateHourlyPrice_givenNotSunday_thenPriceIsDoubleOfNonSundayPrice() {
 
+    /* Exercise 6: Write test case for VipParkingStrategy calculateHourlyPrice
+     * by using PowerMock to mock static method */
 
-    }
+  }
 }
